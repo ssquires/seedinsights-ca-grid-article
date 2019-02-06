07173171a1972f78aa3fc25bb24ff137b23c325e
Sam Squires <sam.h.squires@gmail.com>
2019-02-06 13:01:12 -0800

    added legends to pie charts and noted open issues for all visualizations

--- public/js/article.js
+++ public/js/article.js
@@ -1,21 +1,21 @@
 const pieColorsNow = ['#003F5C', '#68CF6A', '#00A28A'];
-const pieColorsFuture = ['#444444', '#68CF6A'];
+const pieColorsFuture = ['#00A28A', '#68CF6A'];
 
 const presentDayData = [
   {'label': 'Natural Gas', 'value': 43},
-  {'label': 'Solar, Wind, and Other Renewables', 'value': 30},
-  {'label': 'Large Hydro and Nuclear', 'value': 27}
+  {'label': 'Renewables', 'value': 30},
+  {'label': 'Carbon Neutral', 'value': 27}
 ];
 
 const futureData = [
   {'label': 'Carbon Neutral', 'value': 40},
-  {'label': 'Renewable', 'value': 60}
+  {'label': 'Renewables', 'value': 60}
 ];
 
 $(document).ready(function() {
   drawMaps();
-  makePieChart('#pie-present', presentDayData, pieColorsNow, '2017 (Natural gas, large hydro/nuclear, and renewable)');
-  makePieChart('#pie-future', futureData, pieColorsFuture, '2045 (Renewable vs. carbon neutral)');
+  makePieChart('#pie-present', presentDayData, pieColorsNow, '2017');
+  makePieChart('#pie-future', futureData, pieColorsFuture, '2045');
   makeLineChart('#duck-curve',
                 'The Duck Curve',
                 [duckData],
--- public/js/pie-charts.js
+++ public/js/pie-charts.js
@@ -2,10 +2,11 @@
 
 
 
-function makePieChart(svgID, data, pieColors, caption) {
-  let container = d3.select(svgID).append('svg').data([data]).append('g').attr('transform', 'translate(' + 150 + ',' + 150 + ')');
+function makePieChart(svgID, data, pieColors, caption, labelAdjust=0) {
+  let container = d3.select(svgID).append('svg').data([data]).append('g').attr('transform', 'translate(' + 100 + ',' + 150 + ')');
   let pie = d3.pie().value(function(d) { return d.value; });
-  let arc = d3.arc().innerRadius(0).outerRadius(150);
+  let arc = d3.arc().innerRadius(0).outerRadius(100);
+  let outerArc = d3.arc().innerRadius(130 + labelAdjust).outerRadius(150);
 
   let slices = container.selectAll('slice')
     .data(pie).enter()
@@ -17,19 +18,41 @@ function makePieChart(svgID, data, pieColors, caption) {
 
   slices.append('text')
       .attr('transform', function(d){
-          d.innerRadius = 100;
-          d.outerRadius = 150;
           return 'translate(' + arc.centroid(d) + ')';}
       )
       .attr('class', 'pie-label')
       .attr('text-anchor', 'middle')
-      .text( function(d, i) {return data[i].value + '%';});
+      .text( function(d, i) { return data[i].value + '%'; });
+
 
   container.append('text')
     .attr('x', 0)
-    .attr('y', 175)
-    .attr('width', 300)
+    .attr('y', 135)
+    .attr('width', 200)
     .attr('text-anchor', 'middle')
     .attr('class', 'pie-caption')
+    .attr('font-size', 20)
     .text(caption);
+
+    // Make legend
+    let x = 120;
+    let y = -20;
+    for (let i = 0; i < data.length; i++) {
+			container.append('rect')
+				.attr('fill', pieColors[i])
+				.attr('stroke', pieColors[i])
+				.attr('stroke-width', 0.8)
+				.attr('width', 10)
+				.attr('height', 10)
+				.attr('x', x)
+				.attr('y', y);
+
+  		container.append('text')
+  			.html(data[i].label)
+  			.attr('x', x + 15)
+  			.attr('y', y + 9)
+  			.attr('font-size', 14)
+  			.attr('font-family', 'Open Sans');
+  			y += 15;
+  	}
 }

7143017235f937ea9e6153cc99853671a2f478d3
Sam Squires <sam.h.squires@gmail.com>
2019-02-18 18:47:59 -0800

    initial article structure

--- public/js/ca-map.js
+++ public/js/ca-map.js
@@ -4,14 +4,57 @@ function drawMaps() {
     let path = d3.geoPath().projection(projection);
 
     // Create SVG paths to populate map
-    d3.select('#map').selectAll('.ca')
+    // d3.select('#map-present').selectAll('.ca')
+    //     .data(data)
+    //     .enter().append('path')
+    //     .attr('d', path)
+    //     .attr("stroke", "#333")
+    //     .attr("stroke-width", "0.8px")
+    //     .attr("fill", "#333");
+    //makeNodes('#map-present', 'grid-data.csv');
+    // makeLegend('#map-present', 160, 50);
+
+    parseDataFile('present', '#map-present', 'grid-data.csv');
+
+    d3.select('#graph-map-1').selectAll('.ca')
         .data(data)
         .enter().append('path')
         .attr('d', path)
         .attr("stroke", "#333")
         .attr("stroke-width", "0.8px")
         .attr("fill", "#333");
-    makeNodes('#map');
-    makeLegend('#map', 160, 50);
+    // makeLegend('#graph-map-1', 160, 50);
+    parseDataFile('map-1', '#graph-map-1', 'placeholders_nodes.csv', 'placeholders_lines.csv', false);
+
+    d3.select('#graph-map-2-risk').selectAll('.ca')
+        .data(data)
+        .enter().append('path')
+        .attr('d', path)
+        .attr("stroke", "#333")
+        .attr("stroke-width", "0.8px")
+        .attr("fill", "#333");
+    // makeLegend('#graph-map-1', 160, 50);
+    parseDataFile('map-2', '#graph-map-2-risk', 'placeholders_nodes.csv', 'placeholders_lines.csv', true);
+
+    d3.select('#graph-map-3-bad-config').selectAll('.ca')
+        .data(data)
+        .enter().append('path')
+        .attr('d', path)
+        .attr("stroke", "#333")
+        .attr("stroke-width", "0.8px")
+        .attr("fill", "#333");
+    parseDataFile('map-3', '#graph-map-3-bad-config', 'placeholders_nodes_bad_config.csv', 'placeholders_lines_bad_config.csv', true);
+
+    d3.select('#graph-map-4-good-config').selectAll('.ca')
+        .data(data)
+        .enter().append('path')
+        .attr('d', path)
+        .attr("stroke", "#333")
+        .attr("stroke-width", "0.8px")
+        .attr("fill", "#333");
+    parseDataFile('map-4', '#graph-map-4-good-config', 'placeholders_nodes_good_config.csv', 'placeholders_lines_good_config.csv', true);
+
   });
+
+
 }
--- public/js/energy-map.js
+++ public/js/energy-map.js
@@ -4,75 +4,170 @@ let tooltip = d3.select("body")
     		  .style("opacity", 1)
               .style("border-radius", "8px");
 
-let svg;
+function parseDataFile(idPrefix, svgID, dataFile, edgeDataFile, weightedEdges=true) {
+	let nodeList = [];
+	d3.csv(dataFile).then(function(data) {
+		let i = 0;
+		for (let nodeData of data) {
+				node = {};
+				if (nodeData['node number']) {
+					node['id'] = idPrefix + nodeData['node number'];
+				} else {
+					node['id'] = idPrefix + i;
+					i++;
+				}
+				node['long'] = nodeData['X (longitude)'];
+				node['lat'] = nodeData['Y (latitude)'];
 
-function makeNodes(svgID) {
-  let data = d3.csv('grid-data.csv').then(function(data) {
-		svg = svgID;
+				if(nodeData['load']) {
+					node['load'] = nodeData['load'];
+				} else {
+					node['load'] = '';
+				}
+
+				if (nodeData['Solar']) {
+					node['solar'] = nodeData['Solar'];
+				} else {
+					node['solar'] = nodeData['solar'];
+				}
+
+				if (nodeData['Wind']) {
+					node['wind'] = nodeData['Wind'];
+				} else {
+					node['wind'] = nodeData['wind'];
+				}
+
+				if (nodeData['Carbon free (includes biomass)']) {
+					node['carbon_neutral'] = nodeData['Carbon free (includes biomass)'];
+				} else {
+					node['carbon_neutral'] = nodeData['carbon f&n'];
+				}
+
+				if (nodeData['Non-renewables']) {
+					node['non_renewables'] = nodeData['Non-renewables'];
+				} else {
+					node['non_renewables'] = nodeData['nor renewables'];
+				}
+
+				if (nodeData['description']) {
+					node['description'] = nodeData['description'];
+				} else {
+					node['description'] = '';
+				}
+
+				if (nodeData['Total generation']) {
+					node['total_output'] = nodeData['Total generation'];
+				} else {
+					node['total_output'] = Number(node['solar']) + Number(node['wind']) + Number(node['non_renewables']) + Number(node['carbon_neutral'])
+				}
+
+				let solar = node['solar'], wind = node['wind'], cf = node['carbon_neutral'], nr = node['non_renewables'];
+				if (solar > Math.max(wind, cf, nr)) {
+					node['class'] = 'solar node';
+				} else if (wind > Math.max(solar, cf, nr)) {
+					node['class'] = 'wind node';
+				} else if (cf > Math.max(solar, wind, nr)) {
+					node['class'] = 'other node';
+				} else {
+					node['class'] = 'other node';
+				}
+				if (node['load'] < 0) {
+					node['class'] = 'city node';
+				}
+
+				nodeList.push(node);
+		}
+		makeNodes(svgID, nodeList);
+		if (edgeDataFile) {
+				d3.csv(edgeDataFile).then(function(data) {
+					let edgeList = [];
+
+					for (let edgeData of data) {
+						let edge = {};
+						edge['id'] = idPrefix + 'Edge' + edgeData['Line number'];
+						edge['from'] = idPrefix + edgeData['From node #'];
+						edge['to'] = idPrefix + edgeData['To node #'];
+						edge['value'] = edgeData['Value'];
+						edgeList.push(edge);
+					}
+					makeEdges(svgID, edgeList, weightedEdges);
+				});
+		}
+	});
+}
+
+function makeNodes(svgID, data) {
   	// Create a circle on the map for each node.
 	  d3.select(svgID).selectAll('.node')
 	        .data(data.filter(function(d) {
-	            return Number(d['Total generation']) >= 15.0;
+	            return Number(d['total_output']) >= 0.0;
 	        }))
 	        .enter().append('circle')
-					.attr('class', function (d) {
-						let solar = d['Solar'], wind = d['Wind'], cf = d['Carbon free (includes biomass)'], nr = d['Non-renewables'];
-						if (solar > Math.max(wind, cf, nr)) {
-							return 'solar node';
-						} else if (wind > Math.max(solar, cf, nr)) {
-							return 'wind node';
-						} else if (cf > Math.max(solar, wind, nr)) {
-							return 'renewable node';
-						} else {
-							return 'gas node';
-						}
-					})
-	        .attr('cx', function (d) { return projection([d['X (longitude)'], d['Y (latitude)']])[0]})
-	        .attr('cy', function (d) { return projection([d['X (longitude)'], d['Y (latitude)']])[1]})
-	        .attr('r', function (d) { return Math.log(d['Total generation']) / 1.5})
-	        .attr('fill', function (d) {
-						let solar = d['Solar'], wind = d['Wind'], cf = d['Carbon free (includes biomass)'], nr = d['Non-renewables'];
-						if (solar > Math.max(wind, cf, nr)) {
-							return colors['.solar'];
-						} else if (wind > Math.max(solar, cf, nr)) {
-							return colors['.wind'];
-						} else if (cf > Math.max(solar, wind, nr)) {
-							return colors['.renewable'];
-						} else {
-							return colors['.gas'];
-						}
-	        })
-	        .attr('stroke',  function (d) {
-	            let solar = d['Solar'], wind = d['Wind'], cf = d['Carbon free (includes biomass)'], nr = d['Non-renewables'];
-							if (solar > Math.max(wind, cf, nr)) {
-								return colors['.solar'];
-							} else if (wind > Math.max(solar, cf, nr)) {
-								return colors['.wind'];
-							} else if (cf > Math.max(solar, wind, nr)) {
-								return colors['.renewable'];
-							} else {
-								return colors['.gas'];
-							}
-	        })
+					.attr('id', function (d) { return d['id']})
+					.attr('class', function (d) { return d['class']})
+	        .attr('cx', function (d) { return projection([d['long'], d['lat']])[0]})
+	        .attr('cy', function (d) { return projection([d['long'], d['lat']])[1]})
+	        .attr('r', function (d) { return Math.max(Math.log(d['total_output']) / 1.5, 2)})
 					.attr('stroke-width', '0.8px')
-					.attr('fill-opacity', '0.65')
-					.attr('stroke-opacity', '0.85')
+					.attr('fill-opacity', '1')
+					.attr('stroke-opacity', '1')
 	        .on('mouseover', nodeMouseover)
 					.on('mouseout', nodeMouseout);
-	});
+}
+
+function makeEdges(svgID, data, weightedEdges) {
+	d3.select(svgID).selectAll('.edge')
+				.data(data)
+				.enter().append('line')
+				.attr('id', function (d) { return d['id']})
+				.attr('class', function (d) {
+					let r = d['value'];
+					if (!weightedEdges) {
+						return 'edge';
+					}
+					if (r >= 0.9) {
+						return 'risk_90 edge';
+					} else if (r >= 0.8) {
+						return 'risk_80 edge';
+					} else if (r >= 0.7) {
+						return 'risk_70 edge';
+					} else if (r >= 0.6) {
+						return 'risk_60 edge';
+					} else if (r >= 0.5) {
+						return 'risk_50 edge';
+					} else if (r >= 0.4) {
+						return 'risk_40 edge';
+					} else if (r >= 0.3) {
+						return 'risk_30 edge';
+					} else if (r >= 0.2) {
+						return 'risk_20 edge';
+					} else if (r >= 0.1) {
+						return 'risk_10 edge';
+					} else {
+						return 'risk_00 edge';
+					}
+				})
+				.attr('x1', function (d) { return $('#' + d['from']).attr('cx')})
+				.attr('y1', function (d) { return $('#' + d['from']).attr('cy')})
+				.attr('x2', function (d) { return $('#' + d['to']).attr('cx')})
+				.attr('y2', function (d) { return $('#' + d['to']).attr('cy')})
+				.attr('stroke-width', '2px');
+
+		// Raise nodes to be drawn on top of edges.
+		d3.select(svgID).selectAll('.node').raise();
 }
 
 function nodeMouseover(d) {
 	// Determine class of nodes to select
 	let nodeClass;
 	if (this.classList.contains('solar')) {
-		selectClass('.solar');
+		selectClass('.solar', '#' + this.parentNode.id);
 	} else if (this.classList.contains('wind')) {
-		selectClass('.wind');
+		selectClass('.wind', '#' + this.parentNode.id);
 	} else if (this.classList.contains('renewable')) {
-		selectClass('.renewable');
+		selectClass('.renewable', '#' + this.parentNode.id);
 	} else {
-		selectClass('.gas');
+		selectClass('.gas', '#' + this.parentNode.id);
 	}
 
 
@@ -80,46 +175,46 @@ function nodeMouseover(d) {
 
 function nodeMouseout(d) {
 	if (this.classList.contains('solar')) {
-		unselectClass('.solar');
+		unselectClass('.solar', '#' + this.parentNode.id);
 	} else if (this.classList.contains('wind')) {
-		unselectClass('.wind');
+		unselectClass('.wind', '#' + this.parentNode.id);
 	} else if (this.classList.contains('renewable')) {
-		unselectClass('.renewable');	}
+		unselectClass('.renewable', '#' + this.parentNode.id);	}
 	else {
-		unselectClass('.gas');
+		unselectClass('.gas', '#' + this.parentNode.id);
 	}
 }
 
-function selectClass(className) {
+function selectClass(className, svgID) {
 
 	// Gray out other nodes
 	for (let nodeClass of ['.solar', '.wind', '.renewable', '.gas']) {
 		if (nodeClass != className) {
-			d3.select(svg).selectAll(nodeClass).transition('gray').duration(300)
+			d3.select(svgID).selectAll(nodeClass).transition('gray').duration(300)
 				.attr('fill', colors['unselected'])
 				.attr('stroke', colors['unselected']);
 		}
 	}
 
 	// Make selected node class larger
-	d3.select(svg).selectAll(className).raise();
-	d3.select(svg).selectAll(className).transition('big').duration(300)
-		.attr('r', function(d) {return Math.log(d['Total generation'])});
+	d3.select(svgID).selectAll(className).raise();
+	d3.select(svgID).selectAll(className).transition('big').duration(300)
+		.attr('r', function(d) {return Math.log(d['total_output'])});
 }
 
-function unselectClass(className) {
+function unselectClass(className, svgID) {
 
 	// Turn all nodes back to their original colors
 	for (let nodeClass of ['.solar', '.wind', '.renewable', '.gas']) {
-		d3.select(svg).selectAll(nodeClass).transition('color').duration(300)
+		d3.select(svgID).selectAll(nodeClass).transition('color').duration(300)
 			.attr('fill', colors[nodeClass])
 			.attr('stroke', colors[nodeClass]);
 	}
 
 
 	// Make unselected node class smaller
-	d3.select(svg).selectAll(className).transition('small').duration(300)
-		.attr('r', function(d) {return Math.log(d['Total generation']) / 1.5});
+	d3.select(svgID).selectAll(className).transition('small').duration(300)
+		.attr('r', function(d) {return Math.log(d['total_output']) / 1.5});
 }
 
 function makeLegend(svgID, legendX, legendY) {
